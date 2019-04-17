70e9b2b07e115203aabc895ebd743730f0382bc6
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 14:42:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,10 @@ def second_challenge
       :hero => {},
       :hero_friends => []
       }, 
-    :capulet => {
+    :capulet => { :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       
       }}
 
